f852bf18ebbfc243f2ad4d9579b490735e403ced
Kenneth Preston <kenneth.j.preston@gmail.com>
2018-03-22 14:04:09 -0400

    Add files via upload

--- PyBuild.py
+++ PyBuild.py
@@ -1,7 +1,10 @@
-!pip install pytest
-!pip install pytest-cov
-!pip install doctest2
-!pip install -r requirements.txt
-!python -m pytest --junitxml=pytestresults.xml --cov-report=xml --cov-report=html
-!python -m pytest  --junitxml=doctestresultsmod.xml --doctest-modules --doctest-report udiff
-!python -m pytest  --junitxml=doctestresultstxt.xml --doctest-modules --doctest-report udiff
+from subprocess import run
+print(run("pip install pytest"))
+print(run("pip install pytest-cov"))
+print(run("pip install doctest2"))
+print(run("pip install -r requirements.txt"))
+print(run("python -m pytest --junitxml=pytestresults.xml --cov-report=xml --cov-report=html"))
+print(run("python -m pytest  --junitxml=doctestresultsmod.xml --doctest-modules --doctest-report udiff"))
+print(run("python -m pytest  --junitxml=doctestresultstxt.xml --doctest-modules --doctest-report udiff"))
+
+
